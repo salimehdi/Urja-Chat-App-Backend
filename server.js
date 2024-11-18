9b54183850da93fa6b69188ce5a7f95da265416c
salimehdi <salimehdi96@gmail.com>
2024-11-19 00:17:54 +0530

    some changes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
